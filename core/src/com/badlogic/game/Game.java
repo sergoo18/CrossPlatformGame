b12c80a46dde7c8b7c4114d6ee06e778d469ba9a
Сергей Паламарчук <sergeypalamarchuk0512@gmail.com>
2024-02-12 15:29:58 +0200

    Initial commit

---
+++ core/src/com/badlogic/game/Game.java
@@ -0,0 +1,92 @@
+package com.badlogic.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Cubemap;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+
+import net.mgsx.gltf.loaders.gltf.GLTFLoader;
+import net.mgsx.gltf.scene3d.attributes.PBRCubemapAttribute;
+import net.mgsx.gltf.scene3d.lights.DirectionalLightEx;
+import net.mgsx.gltf.scene3d.scene.Scene;
+import net.mgsx.gltf.scene3d.scene.SceneAsset;
+import net.mgsx.gltf.scene3d.scene.SceneManager;
+import net.mgsx.gltf.scene3d.scene.SceneSkybox;
+import net.mgsx.gltf.scene3d.utils.IBLBuilder;
+
+public class Game extends ApplicationAdapter
+{
+	private SceneManager sceneManager;
+	private SceneAsset sceneAsset;
+	private Scene scene;
+	private PerspectiveCamera camera;
+	private Cubemap diffuseCubemap;
+	private Cubemap environmentCubemap;
+	private Cubemap specularCubemap;
+	private SceneSkybox skybox;
+	private DirectionalLightEx light;
+	private FirstPersonCameraController cameraController;
+
+	@Override
+	public void create() {
+		sceneAsset = new GLTFLoader().load(Gdx.files.internal("scene.gltf"));
+		scene = new Scene(sceneAsset.scene);
+		sceneManager = new SceneManager();
+		sceneManager.addScene(scene);
+
+		camera = new PerspectiveCamera(60f, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		float d = .02f;
+		camera.near = d / 1000f;
+		camera.far = 20f;
+		sceneManager.setCamera(camera);
+		cameraController = new FirstPersonCameraController(camera);
+		Gdx.input.setInputProcessor(cameraController);
+
+		light = new DirectionalLightEx();
+		light.direction.set(1, -3, 1).nor();
+		light.color.set(Color.WHITE);
+		sceneManager.environment.add(light);
+
+		IBLBuilder iblBuilder = IBLBuilder.createOutdoor(light);
+		environmentCubemap = iblBuilder.buildEnvMap(1024);
+		diffuseCubemap = iblBuilder.buildIrradianceMap(256);
+		specularCubemap = iblBuilder.buildRadianceMap(10);
+		iblBuilder.dispose();
+
+		sceneManager.setAmbientLight(1f);
+		sceneManager.environment.set(PBRCubemapAttribute.createSpecularEnv(specularCubemap));
+		sceneManager.environment.set(PBRCubemapAttribute.createDiffuseEnv(diffuseCubemap));
+
+		skybox = new SceneSkybox(environmentCubemap);
+		sceneManager.setSkyBox(skybox);
+		scene.animationController.setAnimation("Start_Liftoff", -1);
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		sceneManager.updateViewport(width, height);
+	}
+
+	@Override
+	public void render() {
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		cameraController.update();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		sceneManager.update(deltaTime);
+		sceneManager.render();
+	}
+
+	@Override
+	public void dispose() {
+		sceneManager.dispose();
+		sceneAsset.dispose();
+		environmentCubemap.dispose();
+		diffuseCubemap.dispose();
+		specularCubemap.dispose();
+		skybox.dispose();
+	}
+}
