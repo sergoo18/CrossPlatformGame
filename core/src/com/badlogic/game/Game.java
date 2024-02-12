a4798686a7bda4980532bc9e47d72432dccc1270
Сергей Паламарчук <sergeypalamarchuk0512@gmail.com>
2024-02-12 18:45:58 +0200

    add some files

--- core/src/com/badlogic/game/Game.java
+++ core/src/com/badlogic/game/Game.java
@@ -6,51 +6,63 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Cubemap;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.utils.AnimationController;
 import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+import com.badlogic.gdx.utils.Array;
 
 import net.mgsx.gltf.loaders.gltf.GLTFLoader;
 import net.mgsx.gltf.scene3d.attributes.PBRCubemapAttribute;
 import net.mgsx.gltf.scene3d.lights.DirectionalLightEx;
-import net.mgsx.gltf.scene3d.scene.Scene;
 import net.mgsx.gltf.scene3d.scene.SceneAsset;
-import net.mgsx.gltf.scene3d.scene.SceneManager;
-import net.mgsx.gltf.scene3d.scene.SceneSkybox;
 import net.mgsx.gltf.scene3d.utils.IBLBuilder;
 
 
-public class Game extends ApplicationAdapter
-{
-	private SceneManager sceneManager;
+public class Game extends ApplicationAdapter {
 	private SceneAsset sceneAsset;
-	private Scene scene;
 	private PerspectiveCamera camera;
 	private Cubemap diffuseCubemap;
 	private Cubemap environmentCubemap;
 	private Cubemap specularCubemap;
-	private SceneSkybox skybox;
 	private DirectionalLightEx light;
 	private FirstPersonCameraController cameraController;
+	private ModelBatch modelBatch;
+	private Environment environment;
+	public Array<ModelInstance> instances = new Array<>();
+	private Array<AnimationController> animationControllers = new Array<>();
 
 	@Override
 	public void create() {
 		sceneAsset = new GLTFLoader().load(Gdx.files.internal("scene.gltf"));
-		scene = new Scene(sceneAsset.scene);
-		sceneManager = new SceneManager();
-		sceneManager.addScene(scene);
+		modelBatch = new ModelBatch();
+		environment = new Environment();
+
+		for (float x = -200f; x <= 200f; x += 4f) {
+			for (float z = -200f; z <= 200f; z += 4f) {
+				ModelInstance modelInstance = new ModelInstance(sceneAsset.scene.model);
+				AnimationController animationController = new AnimationController(modelInstance);
+				modelInstance.transform.setToTranslation(x, 0, z);
+				modelInstance.transform.rotate(1,0,0,-90);
+				animationController.setAnimation("Start_Liftoff", -1);
+				animationControllers.add(animationController);
+				instances.add(modelInstance);
+
+			}
+		}
 
 		camera = new PerspectiveCamera(120f, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		float d = .02f;
-		camera.near = d / 1000f;
+		camera.near = .02f / 1000f;
 		camera.far = 20f;
 
-		sceneManager.setCamera(camera);
 		cameraController = new FirstPersonCameraController(camera);
 		Gdx.input.setInputProcessor(cameraController);
 
 		light = new DirectionalLightEx();
 		light.direction.set(1, -3, 1).nor();
 		light.color.set(Color.WHITE);
-		sceneManager.environment.add(light);
+		environment.add(light);
 
 		IBLBuilder iblBuilder = IBLBuilder.createOutdoor(light);
 		environmentCubemap = iblBuilder.buildEnvMap(1024);
@@ -58,36 +70,28 @@ public class Game extends ApplicationAdapter
 		specularCubemap = iblBuilder.buildRadianceMap(1);
 		iblBuilder.dispose();
 
-		sceneManager.setAmbientLight(1f);
-		sceneManager.environment.set(PBRCubemapAttribute.createSpecularEnv(specularCubemap));
-		sceneManager.environment.set(PBRCubemapAttribute.createDiffuseEnv(diffuseCubemap));
-
-		skybox = new SceneSkybox(environmentCubemap);
-		sceneManager.setSkyBox(skybox);
-		scene.animationController.setAnimation("Start_Liftoff", -1);
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		sceneManager.updateViewport(width, height);
+		environment.set(PBRCubemapAttribute.createSpecularEnv(specularCubemap));
+		environment.set(PBRCubemapAttribute.createDiffuseEnv(diffuseCubemap));
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		float deltaTime = Gdx.graphics.getDeltaTime();
+		for (AnimationController animationController : animationControllers) {
+			animationController.update(deltaTime);
+		}
 		cameraController.update();
-		sceneManager.update(deltaTime);
-		sceneManager.render();
+		modelBatch.begin(camera);
+		modelBatch.render(instances, environment);
+		modelBatch.end();
 	}
 
 	@Override
 	public void dispose() {
-		sceneManager.dispose();
 		sceneAsset.dispose();
 		environmentCubemap.dispose();
 		diffuseCubemap.dispose();
 		specularCubemap.dispose();
-		skybox.dispose();
 	}
 }
---
+++ core/src/com/badlogic/scene/Scene.java
@@ -0,0 +1,4 @@
+package com.badlogic.scene;
+
+public class Scene {
+}
